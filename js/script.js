3ddb95128a3f2bb51193a30a0d4280fcc190ecb0
yasaminpou <83372444+yasaminpou@users.noreply.github.com>
2021-05-21 10:26:41 -0700

    Update script.js
    If applied this commit will add function for menu bar to the code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("It is lovely to have you here");
+https://code.jquery.com/jquery-3.6.0.min.js
+function menuToggle () {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
